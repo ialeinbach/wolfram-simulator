381c0028a85016736a89b691e7486d759b89988a
Ian <inleinbach@gmail.com>
2017-10-14 22:36:57 -0400

    Added use of flags package for command line arguments, added use of termbox-go for terminal dimensions detection (cross-platform), use 256 instead of -1 for ruleId special value, some renaming for clarity

--- wolframsimulator.go
+++ wolframsimulator.go
@@ -2,80 +2,92 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"strconv"
-	"strings"
 	"time"
 	"errors"
 	"log"
+	"flag"
+	"github.com/nsf/termbox-go"
 )
 
+// minimum height of terminal required
+const MIN_HEIGHT = 4
 
-// Because each bit depends on the above bit and those adjacent, each
+// receive values from flags
+var nRows, displayWidth, ruleId int
+
+// hold terminal dimensions
+var terminalWidth, terminalHeight int
+
+// Because each bit depends on the above bit and those adjacent, a
 // row of width n is determined by a row of width n+2. Given
 // nRows, arrWidth holds size of initial row needed to completely
-// determine nRows of width dispWidth. arrMiddle holds middle index
-func getSizeInfo(nRows, dispWidth int) (arrWidth, arrMiddle int) {
-        // offset arrWidth by dispWidth so the last row is
-        // dispWidth wide rather than 0 wide
-        //
-        // dispWidth guaranteed to be odd, so arrWidth will
-        // always be odd i.e. have a middle element
-	arrWidth = 2 * (nRows - 1) + dispWidth
+// determine nRows of width displayWidth. arrMiddle holds middle index
+// of arrWidth
+func getSizeInfo() (arrWidth, arrMiddle int) {
+	// offset arrWidth by displayWidth so
+	// the last row is displayWidth wide
+	//
+	// displayWidth guaranteed to be odd, so arrWidth will
+	// always be odd i.e. have a middle element
+	arrWidth = 2 * (nRows - 1) + displayWidth
 	arrMiddle = (arrWidth - 1) / 2
 	return
 }
 
-//Return printable character to display
-func getDispString(value byte) (dispString string) {
+// return printable character to represent a bit value
+func getDisplayString(value byte) (displayString string) {
 	if value == 0 {
-		dispString = " "
+		displayString = " "
 	} else {
-		dispString = "#"
+		displayString = "#"
 	}
 
 	return
 }
 
-// Rule encoded as single byte where each bit
-// represents output for an input of a bit's index (0-7)
-// when expressed as 3-bit binary number (000-111)
+// rules encoded as single byte where each bit
+// represents output resulting from input of a
+// bit's index (0-7) when expressed as 3-bit
+// binary number (000-111)
 func applyRule(input, rule byte) byte {
+	// apply bit mask over bottom three bits of input
+	// to prevent over-shifting
 	return (rule >> (input & 7)) & 1
 }
 
-// initial row has all 0s except a single 1 in the
-// middle to start
-func initialRow(nRows, dispWidth int) []byte {
-	width, middle := getSizeInfo(nRows, dispWidth)
-	firstRow := make([]byte, width)
-	firstRow[middle] = 1
+// initial row has all 0s except a
+// single 1 in the middle
+func generateInitialRow() (initialRow []byte) {
+	width, middle := getSizeInfo()
 
-	return firstRow
+	initialRow = make([]byte, width)
+	initialRow[middle] = 1
+
+	return
 }
 
 // returns indices start, end such that row[start:end] contains
-// middle dispWidth elements of entire row
+// middle displayWidth elements of entire row
 //
 // rows are guaranteed to have odd length, so there is
 // always a middle subset of elements
-func getDispBounds(arrWidth, dispWidth int) (start, end int) {
-	start = (arrWidth - dispWidth) / 2
-	end = start + dispWidth
+func getDisplayBounds(arrWidth int) (start, end int) {
+	start = (arrWidth - displayWidth) / 2
+	end = start + displayWidth
 
 	return
 }
 
 // given a slice of a row, insert into a byte
-// to be processed by applyRule
+// to be processed by applyRule()
 func parseInput(previousInput []byte) byte {
 	length := len(previousInput)
 
 	if length != 3 {
-		log.Fatal(errors.New("parseInput given slice of invalid length"))
+		log.Fatal(errors.New("parseInput given slice with invalid length"))
 	}
 
+	// initialized to 0
 	var input byte
 
 	for i := 0; i < length; i++ {
@@ -87,156 +99,122 @@ func parseInput(previousInput []byte) byte {
 }
 
 // generates new row based on previous row and rule
-func generateRow(previousRow []byte, rule byte) []byte {
+func generateNextRow(previousRow []byte, rule byte) (nextRow []byte) {
 	arrWidth := len(previousRow) - 2
-	currentRow := make([]byte, arrWidth)
+	nextRow = make([]byte, arrWidth)
 
-        // row sizes are such that expressions with i will not be out of bounds
+	// iterate across previousRow
+	// generate nextRow according to rule
 	for i := 0; i < arrWidth; i++ {
-		currentRow[i] = applyRule(parseInput(previousRow[i:i+3]), rule)
+		nextRow[i] = applyRule(parseInput(previousRow[i:i+3]), rule)
 	}
 
-	return currentRow
+	return
 }
 
-// prints row
-func printRow(row []byte, dispWidth int) {
-	length := len(row)
-	i, end := getDispBounds(length, dispWidth)
+// print at most the central displayWidth elements of a row
+func printRow(row []byte) {
+	arrWidth := len(row)
+	i, end := getDisplayBounds(arrWidth)
 
 	for ; i < end; i++ {
-		fmt.Printf("%v", getDispString(row[i]))
+		fmt.Printf("%s", getDisplayString(row[i]))
 	}
 
 	fmt.Println()
 }
 
-// print simple label for a given rule
-func printRuleLabel(ruleId byte) {
-	fmt.Printf("\n====================\n")
-	fmt.Printf("||    Rule %3d    ||\n", ruleId)
-	fmt.Printf("====================\n\n")
+// print simple label for a rule
+func printRuleLabel() {
+	fmt.Printf(".------------------.\n")
+	fmt.Printf("|     Rule %3d     |\n", ruleId)
+	fmt.Printf("`------------------'\n")
 }
 
-// simulates rule ruleId for nRows rows of width dispWidth
-func dispRule(ruleId, nRows, dispWidth int) {
-	rule := byte(ruleId)
-	printRuleLabel(rule)
+// validates ruleId, nRows, and displayWidth
+func validateParameters() error {
+	//ruleId = 256 is a special value, see main() for behavior
+	if ruleId < 0 || ruleId > 256 {
+		return errors.New("Invalid ruleId: 0 <= ruleId < 256 or rule = 256 to print every rule, one every second")
+	} else if displayWidth > terminalWidth {
+		return errors.New(fmt.Sprintf("Invalid displayWidth: displayWidth <= terminalWidth(%d)", terminalWidth))
+	} else if nRows <= 0 {
+		return errors.New("Invalid nRows: nRows > 0")
+	}
 
-	currentRow := initialRow(nRows, dispWidth)
-	printRow(currentRow, dispWidth)
+	return nil
+}
+
+// simulates rule ruleId for nRows rows with a width of displayWidth
+// note: assumes validated parameters (ruleId, nRows, displayWidth)
+//   \__
+//      `--> validateParameters() called in init()
+func displayRule() {
+	rule := byte(ruleId)
+	printRuleLabel()
+
+	currentRow := generateInitialRow()
+	printRow(currentRow)
 
 	for i := 1; i < nRows; i++ {
-		currentRow = generateRow(currentRow, rule)
-		printRow(currentRow, dispWidth)
+		currentRow = generateNextRow(currentRow, rule)
+		printRow(currentRow)
 	}
 }
 
-func getTerminalDimensions() (int, int, error) {
-	cmd := exec.Command("stty", "size")
-	cmd.Stdin = os.Stdin
-	out, cmdErr := cmd.Output()
-
-	if cmdErr != nil {
-		return 0, 0, cmdErr
+// clear terminal and detect terminal dimensions
+func initializeTerminal() error {
+	// termbox.Init() also clears terminal
+	if err := termbox.Init(); err != nil {
+		return err
 	}
 
-	dimensions := strings.Split(strings.TrimSpace(string(out)), " ")
+	// only need to close if *succesfully* initialized
+	defer termbox.Close()
 
-	height, atoiErr1 := strconv.Atoi(dimensions[0])
-	width, atoiErr2 := strconv.Atoi(dimensions[1])
+	terminalWidth, terminalHeight = termbox.Size()
 
-	if atoiErr1 != nil {
-		return 0, 0, atoiErr1
+	if terminalHeight < MIN_HEIGHT {
+		return errors.New(fmt.Sprintf("Terminal window height must be at least %d.", MIN_HEIGHT))
 	}
 
-	if atoiErr2 != nil {
-		return 0, 0, atoiErr2
-	}
-
-	return width, height, nil
+	return nil
 }
 
-func main() {
-	args := os.Args
-	numArgs := len(args)
-
-	width, height, err := getTerminalDimensions()
-
-	if err != nil {
+func init() {
+	// also detects terminal dimensions
+	if err := initializeTerminal(); err != nil {
 		log.Fatal(err)
 	}
 
-	if height < 6 {
-		log.Fatal(errors.New(fmt.Sprintf("Terminal window height (%d) must be at least 6.\n", height)))
+	// usage messages for flags
+	const (
+		usgNRows = "number of rows for which to simulate rule(s)"
+		usgDisplayWidth = "width of rows"
+		usgRuleId = "valid rule to be simulated or 256 to display all rules, one every second"
+	)
+
+	// define flags
+	flag.IntVar(&nRows, "rows", terminalHeight - MIN_HEIGHT, usgNRows)
+	flag.IntVar(&displayWidth, "width", terminalWidth, usgDisplayWidth)
+	flag.IntVar(&ruleId, "rule", 256, usgRuleId)
+
+	flag.Parse()
+
+	// ensure valid parameters for displayRule()
+	if err := validateParameters(); err != nil {
+		log.Fatal(err)
 	}
+}
 
-	// initialize to invalid values
-        // if unchanged, will be invalid
-	nRows, ruleId, dispWidth := -1, -1, -1
-
-	switch numArgs {
-		// numArgs == 1 (i.e. only program name)
-		// print every rule, one per second
-		case 1:
-			for i := 0; i < 256; i++ {
-				dispRule(i, height - 5, width)
-				time.Sleep(time.Second)
-			}
-
-			// successful exit
-			os.Exit(0)
-
-		// numArgs > 4
-		default:
-			log.Fatal(errors.New("Too many arguments."))
-
-                // will extract only arguments that exist using fallthroughs
-
-		// numArgs == 4 --> extract all except program name from os.args
-		case 4:
-			dispWidth, err = strconv.Atoi(os.Args[3])
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fallthrough
-
-		// numArgs == 3 --> extract all except program name, dispWidth from os.args
-		case 3:
-			nRows, err = strconv.Atoi(os.Args[2])
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fallthrough
-
-		// numArgs == 2 --> extract all except program name, dispWidth, nRows from os.args
-		case 2:
-			ruleId, err = strconv.Atoi(os.Args[1])
-
-			if err != nil {
-				log.Fatal(err)
-			}
+func main() {
+	// special value used to print all rules, one every second
+	if ruleId == 256 {
+		for ruleId = 0; ruleId < 256; ruleId++ {
+			displayRule()
+			time.Sleep(time.Second)
+		}
+	} else {
+		displayRule()
 	}
-
-	// check if within range (also implicitly whether they
-	// were assigned values to begin with)
-	//
-	// if invalid(if unassigned, guaranteed to be invalied),
-        // use known valid values
-	if dispWidth > 0 {
-            if dispWidth % 2 == 0 {
-		dispRule(ruleId, nRows, dispWidth)
-            } else {
-                dispRule(ruleId, nRows, dispWidth - 1)
-            }
-	} else if nRows > 0 {
-		dispRule(ruleId, nRows, width)
-	} else if ruleId >= 0 {
-		dispRule(ruleId, height - 6, width)
-	}
-
 }
